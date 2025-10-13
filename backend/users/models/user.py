7626ef3ea5b485690b3863a7fab2fd6724781441
Bach Nguyen <xuanbacha1gv@gmail.com>
2025-10-13 14:39:51 +0800

    Use AbstractUser for User, fix username to be UUID by default, add fields to UserProfile

--- backend/users/models/user.py
+++ backend/users/models/user.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
 
 class User(AbstractUser):
@@ -8,8 +9,18 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
+    # Set username to a unique UUID
+    username = models.CharField(
+        max_length=150, 
+        unique=True, 
+        default=uuid.uuid4
+    )
+
+    # Add gender here
+    
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    # Add username to REQUIRED_FIELDS
+    REQUIRED_FIELDS = ['username']
     
     def __str__(self):
         return self.email
